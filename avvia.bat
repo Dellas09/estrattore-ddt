f40e4972c74ac0ebfa026639ded7252f24838c62
Dellas09 <tread9.79@gmail.com>
2026-03-08 17:14:48 +0100

    Update avvia.bat

--- avvia.bat
+++ avvia.bat
@@ -1,3 +1,7 @@
 @echo off
 cd /d "%~dp0"
+echo Avvio Ollama...
+start "" "C:\Users\Alessandro\Desktop\mailhelper\ollama-windows-amd64\ollama.exe" serve
+timeout /t 3 /nobreak >nul
+echo Avvio Estrattore DDT...
 python estrai_ddt.py
