b5a12b08324d0371a37e5cf0797e93cfeba26439
Dellas09 <tread9.79@gmail.com>
2026-03-08 12:36:11 +0100

    Add files via upload

---
+++ avvia.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "%~dp0"
+python estrai_ddt.py
---
+++ installa.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Installazione dipendenze - Estrattore DDT
+pip install pymupdf easyocr fpdf2 requests
+echo.
+echo Fatto! Ora avvia: avvia.bat
+pause
